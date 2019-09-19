60058482651d717324b2086c1b25d5cc3d32ec75
ursgraf <urs.graf@ntb.ch>
2019-09-19 16:31:47 +0200

    Rename demo application

--- demoApp/main.cpp
+++ demoApp/main.cpp
@@ -16,59 +16,48 @@
  * logs a PeripheralInput (an encoder) periodically.
  */
 
+double period = 0.1;
 
-class MotorControlSystem {
-  public:
-    MotorControlSystem(eeros::control::TimeDomain& td):
-    enc1{"enc1"},
-    motor1{"motor1"},
-    const1{1.0}
-    {
-      motor1.getIn().connect(const1.getOut());
+class ControlSystem {
+ public:
+  ControlSystem() : td("Main time domain", period, true), enc1{"enc1"}, motor1{"motor1"}, const1{1.0} {
+    motor1.getIn().connect(const1.getOut());
+    td.addBlock(enc1);
+    td.addBlock(const1);
+    td.addBlock(motor1);
+    eeros::Executor::instance().add(td);
+  };
 
-      td.addBlock(enc1);
-      td.addBlock(const1);
-      td.addBlock(motor1);
-    };
-
-
-    eeros::control::Signal<double>& getEncoderSignal(){
-      return enc1.getOut().getSignal();
-    }
-
- 
-  private:
-    eeros::control::PeripheralInput<double> enc1;
-    eeros::control::PeripheralOutput<double> motor1;
-    eeros::control::Constant<double> const1;
+  eeros::control::PeripheralInput<double> enc1;
+  eeros::control::PeripheralOutput<double> motor1;
+  eeros::control::Constant<double> const1;
+  eeros::control::TimeDomain td;
 };
 
 
 
 class MySafetyProperties : public eeros::safety::SafetyProperties {
-  public:
-    MySafetyProperties(eeros::logger::Logger& loggerRef):
-    slRun{"Safety Level RUN"},
-    slStop{"Safety Level STOP"},
-    seRun{"Goto SL RUN"},
-    seStop{"Goto SL STOP"},
-    log{loggerRef}
-    {
-      addLevel(slRun);
-      addLevel(slStop);
+ public:
+  MySafetyProperties(eeros::logger::Logger& loggerRef)
+    : slRun{"Safety Level RUN"},
+      slStop{"Safety Level STOP"},
+      seRun{"Goto SL RUN"},
+      seStop{"Goto SL STOP"},
+      log{loggerRef} {
+    addLevel(slRun);
+    addLevel(slStop);
 
-      slRun.addEvent(seStop, slStop);
-      slStop.addEvent(seRun, slRun);
+    slRun.addEvent(seStop, slStop);
+    slStop.addEvent(seRun, slRun);
 
-      setEntryLevel(slRun);
-    }
+    setEntryLevel(slRun);
+  }
 
-  private:
-    eeros::safety::SafetyLevel slRun, slStop;
-    eeros::safety::SafetyEvent seRun, seStop;
-    eeros::logger::Logger& log;
+ private:
+  eeros::safety::SafetyLevel slRun, slStop;
+  eeros::safety::SafetyEvent seRun, seStop;
+  eeros::logger::Logger& log;
 };
-
 
 void signalHandler(int signum) {
   eeros::safety::SafetySystem::exitHandler();
@@ -84,54 +73,48 @@ int main(int argc, char **argv) {
   using namespace eeros::hal;
   using namespace eeros::safety;
 
-  double period{0.1};
-
   StreamLogWriter streamWriter{std::cout};
   Logger::setDefaultWriter(&streamWriter);
   Logger log{};
-
-  log.info() << "MotorControlSystem started.";
+  log.info() << "demo started.";
 
   HAL::instance().readConfigFromFile(&argc, argv);
 
   signal(SIGINT, signalHandler);
 
   MySafetyProperties safetyProperties{log};
-  SafetySystem safetySystem{safetyProperties, period};
-
-  TimeDomain td{"timeDomain", period, true};
-  MotorControlSystem motorCS{td};
+  SafetySystem ss{safetyProperties, period};
+  ControlSystem cs;
 
   int cycleCounter{};
   bool success{false};
 
   Lambda lambdafunc{[&](){
-    log.info() << "encoder counts: " << motorCS.getEncoderSignal();
+    log.info() << "encoder counts: " << cs.enc1.getOut().getSignal();
 
-    if(motorCS.getEncoderSignal().getValue() > 31.42){ // 5*2PI
-       eeros::Executor::stop();
-       success = true;
-    }else {
+    if (cs.enc1.getOut().getSignal().getValue() > 31.42) { // 5*2PI
+      eeros::Executor::stop();
+      success = true;
+    } else {
       cycleCounter++;
     }
 
-    if(cycleCounter > 100) { //100*0.2s = 20s
-       log.error() << "timeout.";
-       eeros::Executor::stop();
+    if (cycleCounter > 100) { //100*0.2s = 20s
+      log.error() << "timeout.";
+      eeros::Executor::stop();
     }
   }};
 
   Periodic periodic{"periodic", 0.2, lambdafunc};
 
   auto& executor = eeros::Executor::instance();
-  executor.setMainTask(safetySystem);
-  executor.add(td);
+  executor.setMainTask(ss);
   executor.add(periodic);
   executor.run(); // blocking call
 
-  if(!success) {
-   log.error() << "failed. Exit with status 1.";
-   return 1;
+  if (!success) {
+    log.error() << "failed. Exit with status 1.";
+    return 1;
   }
 
   log.info() << "Application terminated with success.";
